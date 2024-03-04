23464bc83cb171f60a901d41f378a5ba6e9ce0d6
JulienRebillon <julienrebillon@yahoo.fr>
2024-03-04 08:30:51 +0100

    html/css header fixed sauf logo

--- scripts/recipeCard.js
+++ scripts/recipeCard.js
@@ -4,7 +4,9 @@ async function fetchRecipes() {
     try {
         // Import recipes from the recipes.js file
         const { recipes } = await import('../assets/json/recipes.js');
+        
         return recipes;
+        
     } catch (error) {
         console.error('Error fetching recipes:', error);
         return null;
@@ -29,20 +31,19 @@ function RecipeCard(recipe) {
         let cardHTML = `
             <div class="card">
                 <img src="${this.image}" alt="${this.name}" class="card-img-top">
+                <p class="card-text">Time: ${this.time} minutes</p>
                 <div class="card-body">
                     <h2 class="card-title">${this.name}</h5>
-                    <p class="card-text">Servings: ${this.servings}</p>
-                    <p class="card-text">Time: ${this.time} minutes</p>
+                    <h3 class="rubrique">RECETTE<h3>                    
                     <p class="card-text">Description: ${this.description}</p>
-                    <p class="card-text">Appliance: ${this.appliance}</p>
-                    <p class="card-text">Ustensils: ${this.ustensils.join(', ')}</p>
-                    <h6>Ingredients:</h6>
+                    
+                    <h3>INGREDIENTS</h3>
                     <ul>`;
-        this.ingredients.forEach(ingredient => {
-            cardHTML += `
-                        <li>${ingredient.quantity ? ingredient.quantity : ''} ${ingredient.unit ? ingredient.unit : ''} ${ingredient.ingredient}</li>`;
-        });
-        cardHTML += `
+                        this.ingredients.forEach(ingredient => {
+                            cardHTML += `
+                                <li>${ingredient.quantity ? ingredient.quantity : ''} ${ingredient.unit ? ingredient.unit : ''} ${ingredient.ingredient}</li>`;
+                        });
+                        cardHTML += `
                     </ul>
                 </div>
             </div>`;
@@ -53,13 +54,15 @@ function RecipeCard(recipe) {
 document.addEventListener('DOMContentLoaded', function() {
     fetchRecipes().then(recipes => {
         if (recipes) {
+            const recipeList = document.querySelector('.recipeList'); // Select the recipeList section
+            console.log(recipeList);
             recipes.forEach(recipe => {
                 // Adjust image URL path
                 recipe.image = `assets/images/${recipe.image}`;
 
                 const card = new RecipeCard(recipe);
                 const cardHTML = card.generateCardHTML();
-                document.getElementById('recipeCards').innerHTML += cardHTML;
+                recipeList.innerHTML += cardHTML; // Append the card HTML to the recipeList section
             });
         }
     });
